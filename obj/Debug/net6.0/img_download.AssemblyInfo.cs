fd5ae883a5f2909a49211b0f22b5ada8b670d153
zanox75sswq <113130630+zanox75sswq@users.noreply.github.com>
2025-01-20 01:07:14 +0700

    231
    3131332

--- obj/Debug/net6.0/img_download.AssemblyInfo.cs
+++ obj/Debug/net6.0/img_download.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("img_download")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a89bbf5946f200746d4c0edd974d788918076d")]
 [assembly: System.Reflection.AssemblyProductAttribute("img_download")]
 [assembly: System.Reflection.AssemblyTitleAttribute("img_download")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
